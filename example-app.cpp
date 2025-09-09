95820df85c65ee73a06ce626f0f402bd11912556
AnFunctionArray <sasho648@gmail.com>
2025-09-09 22:13:25 +0300

    Add files via upload

--- example-app.cpp
+++ example-app.cpp
@@ -2364,7 +2364,7 @@ int main(int, char**) {
 									runlr2 = runlrb2;
 									runlr3 = runlrb3;//0.00000166666 * loss2.item().toFloat();
 									runlradv = 100.;
-									rfgridlst = tolrnll2.clone().detach();//abvsgrids.toType(c10::ScalarType::Bool).bitwise_and(tolrnll2.clone().detach().toType(c10::ScalarType::Bool)).toType(c10::ScalarType::Float);//rfgrid.clone().detach();
+									rfgridlst = reswillwino.defined() ? reswillwino1.clone().detach().reshape_as(rfgridlst) : rfgridlst;//tolrnll2.clone().detach();//abvsgrids.toType(c10::ScalarType::Bool).bitwise_and(tolrnll2.clone().detach().toType(c10::ScalarType::Bool)).toType(c10::ScalarType::Float);//rfgrid.clone().detach();
 
 									totrainllst = test2->mem.detach().clone();
 
